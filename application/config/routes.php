fc6a320d37b702c6adbeb4050d1f9eb139970b5a
weezykon <weezykon@gmail.com>
2018-03-17 15:02:27 +0100

    removed glogin route

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,7 +3,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $route['default_controller'] = 'Dashboard/index';
 $route['hashtag'] = 'Dashboard/hashtag';
-$route['glogin'] = 'Dashboard/glogin';
-$route['gcallback'] = 'Dashboard/gcallback';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
